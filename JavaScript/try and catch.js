9102faeed974e5f34134c3dd99c3e92c94622843
Usman Javed <usmanjaved0816@gmail.com>
2024-10-04 20:04:52 +0500

    try and catch in js

--- JavaScript/try and catch.js
+++ JavaScript/try and catch.js
@@ -1,4 +1,4 @@
-//   getters and setters in js 
+//  try and catch 
 
 const person = {
     firstname : 'mosh',
@@ -7,10 +7,21 @@ const person = {
         return ` ${person.firstname}  ${person.lastname}`
     },
     set fullname (value) {
+        if (typeof value !== 'string')
+        throw new Error ('value is not a string');
         const parts = value.split(' ');
+        if (parts.length !==2)
+        throw new Error (' enter first and last name ')
         this.firstname = parts[0];
         this.lastname = parts[1];
     }
 }
+
+try {
 person.fullname = 'elsa khan';
+
+}
+catch (err){
+    alert(err);
+}
 console.log(person);
