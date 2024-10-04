2dcb7ecdb4a41840c96999aa25922db7d632b17d
Usman Javed <usmanjaved0816@gmail.com>
2024-10-04 18:34:25 +0500

    function decleration in js

---
+++ JavaScript/function decleration vs expressions.js
@@ -0,0 +1,19 @@
+
+
+// function decleration 
+  function walk () {
+    console.log('walk');
+  }
+
+// anonymous function expression 
+
+ const run = function () {
+      console.log('run');
+ };
+
+// calling with other name  
+
+let another = run ;
+
+run ();
+another();
