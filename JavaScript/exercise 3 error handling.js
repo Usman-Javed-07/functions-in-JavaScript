01730991f24e376838d0f878db6a678aabf6f889
Usman Javed <usmanjaved0816@gmail.com>
2024-10-05 14:37:01 +0500

    slove exercise 3

---
+++ JavaScript/exercise 3 error handling.js
@@ -0,0 +1,18 @@
+//  error handling 
+try {
+const  number = [1,2,3,4,5,5];
+const count = countOccrrence (null , 5);
+console.log(count);
+}
+catch (e){
+    console.log(e.message);
+}
+function countOccrrence ( array , searchelement) {
+if (!Array.isArray(array))
+    throw new Error ('invalid input');
+return array.reduce((accumalator , current) => {
+   const occrence = (current === searchelement)
+   console.log(accumalator , current , searchelement)
+   return accumalator + occrence;
+},0)
+}
